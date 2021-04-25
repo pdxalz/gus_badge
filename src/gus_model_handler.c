a2b21a946987ef76cbec6eeb6a7d27b148148dd6
pdxalz <pdxalz@users.noreply.github.com>
2021-04-24 21:19:00 -0700

    new model starting to work.

--- src/gus_model_handler.c
+++ src/gus_model_handler.c
@@ -8,126 +8,61 @@
 #include <bluetooth/mesh/models.h>
 #include <dk_buttons_and_leds.h>
 #include "gus_model_handler.h"
+#include "gus_cli.h"
 
-
-////////////////// FORWORDS
-//gus
-static void gus_set(struct bt_mesh_lvl_srv *srv, struct bt_mesh_msg_ctx *ctx,
-		    const struct bt_mesh_lvl_set *set,
-		    struct bt_mesh_lvl_status *rsp);
-
-static void gus_get(struct bt_mesh_lvl_srv *srv, struct bt_mesh_msg_ctx *ctx,
-		    struct bt_mesh_lvl_status *rsp);
-
-
-
-////////////////// STRUCTS
-//gus
-static const struct bt_mesh_lvl_srv_handlers gus_handlers = {
-	.set = gus_set,
-	.get = gus_get,
-};
-
-struct gus_ctx {
-	struct bt_mesh_lvl_srv srv;
-	uint32_t remaining;
-	uint16_t value;
-};
-
-static struct gus_ctx gus_ctx = {
-		.srv = BT_MESH_LVL_SRV_INIT(&gus_handlers),
-};
 
 uint16_t blinker = 0;
 uint16_t get_blinker(void) {return blinker;}
 void set_blinker(uint16_t val) {blinker = val;}
 uint16_t dec_blinker(void) {return --blinker;}
 
-enum gus_mode
-{
-  gm_alert,
-  gm_healthy,
-  gm_infected,
-  gm_innoculated,
-  gm_red_green,
-  gm_blue_green,
-  gm_red_blue,
-  gm_black
-};
-
 ///////////////////// PROCEDURES
-static void gus_status(struct gus_ctx *gus, struct bt_mesh_lvl_status *status)
+
+static void display_health(enum bt_mesh_gus_cli_state state)
 {
-	status->target = gus->value;
-}
-
-
-static void gus_set(struct bt_mesh_lvl_srv *srv, struct bt_mesh_msg_ctx *ctx,
-		    const struct bt_mesh_lvl_set *set,
-		    struct bt_mesh_lvl_status *rsp)
-{
-	struct gus_ctx *gus = CONTAINER_OF(srv, struct gus_ctx, srv);
-
-        set_blinker( set->lvl == gm_alert ? 100 : 0);
-	if (set->lvl != gus->value) 
-        {
-    
-            switch((uint16_t)set->lvl) {
-            case gm_alert:
- //               set_blinker(100);
-            break;
-           
-            case gm_healthy:
-            case 0x2000:
-                dk_set_leds(0b00110110);
+        set_blinker( state == BT_MESH_GUS_CLI_IDENTIFY ? 100 : 0);
+	if (state != BT_MESH_GUS_CLI_IDENTIFY) 
+        {	   
+            switch((uint16_t)state) {
+             case BT_MESH_GUS_CLI_HEALTHY:
+                dk_set_leds(0b00100100);
             break;
             
-            case gm_infected:
-            case 0x4000:
+            case BT_MESH_GUS_CLI_MASKED:
+               dk_set_leds(0b00100110);
+            break;
+                          
+            case BT_MESH_GUS_CLI_VACCINATED:
+                dk_set_leds(0b00100101);
+            break;
+
+            case BT_MESH_GUS_CLI_VACCINATED_MASKED:
+                dk_set_leds(0b00100111);
+            break;
+
+            case BT_MESH_GUS_CLI_INFECTED:
                 dk_set_leds(0b00010010);
             break;
 
-            case gm_innoculated:
-            case 0x6000:
-                dk_set_leds(0b00001001);
+            case BT_MESH_GUS_CLI_VACCINATED_INFECTED:
+                dk_set_leds(0b00010011);
             break;
 
-            case gm_red_green:
-            case 0x8000:
-                dk_set_leds(0b00010100);
-            break;
-
-            case gm_blue_green:
-            case 0xa000:
-                dk_set_leds(0b00001100);
-            break;
-
-            case gm_red_blue:
-            case 0xc000:
-                dk_set_leds(0b00010001);
-            break;
+            case BT_MESH_GUS_CLI_INFECTED_MASKED:
+                dk_set_leds(0b00010110);
+            break;               
                           
-            case gm_black:
-            case 0xe000:
-                dk_set_leds(0b00000000);
+            case BT_MESH_GUS_CLI_VACCINATED_MASKED_INFECTED:
+                dk_set_leds(0b00010111);
             break;               
             }
-            gus->value = set->lvl;
         }
-
-	if (rsp) {
-		gus_status(gus, rsp);
-	}
 }
 
 
-static void gus_get(struct bt_mesh_lvl_srv *srv, struct bt_mesh_msg_ctx *ctx,
-		    struct bt_mesh_lvl_status *rsp)
-{
-	struct gus_ctx *gus = CONTAINER_OF(srv, struct gus_ctx, srv);
-
-	gus_status(gus, rsp);
-}
+//////////////////////////////
+//  Health server
+//////////////////////////////
 
 static void attention_on(struct bt_mesh_model *mod)
 {
@@ -151,14 +86,62 @@ static struct bt_mesh_health_srv health_srv = {
 
 BT_MESH_HEALTH_PUB_DEFINE(health_pub, 0);
 
+// ******************************************************************************
+// ***************************** GUS model setup *******************************
+// ******************************************************************************
+
+static void handle_gus_start(struct bt_mesh_gus_cli *gus)
+{
+    printk("started gus\n");
+	
+}
+
+static void handle_gus_set_state(struct bt_mesh_gus_cli *gus,
+				 struct bt_mesh_msg_ctx *ctx,
+				 enum bt_mesh_gus_cli_state state)
+{
+    printk("set state:  %x %d\n", ctx->addr, state);
+    display_health(state);
+}
+
+
+
+static const struct bt_mesh_gus_cli_handlers gus_handlers = {
+	.start = handle_gus_start,
+	.set_state = handle_gus_set_state,
+};
+
+static struct bt_mesh_gus_cli gus = {
+	.handlers = &gus_handlers,
+};
+
+
+
+
+
+#if 0
 static struct bt_mesh_elem elements[] = {
 	BT_MESH_ELEM(
 		1, BT_MESH_MODEL_LIST(
 			BT_MESH_MODEL_CFG_SRV,
 			BT_MESH_MODEL_HEALTH_SRV(&health_srv, &health_pub),
-                        BT_MESH_MODEL_LVL_SRV(&gus_ctx.srv)),
+                        BT_MESH_MODEL_GUS_CLI(&gus)),
 		BT_MESH_MODEL_NONE),
 };
+#else
+static struct bt_mesh_elem elements[] = {
+	BT_MESH_ELEM(
+		1,
+		BT_MESH_MODEL_LIST(
+			BT_MESH_MODEL_CFG_SRV,
+			BT_MESH_MODEL_HEALTH_SRV(&health_srv, &health_pub)),
+		BT_MESH_MODEL_LIST(BT_MESH_MODEL_GUS_CLI(&gus))),
+};
+#endif
+
+
+
+
 
 static const struct bt_mesh_comp comp = {
 	.cid = CONFIG_BT_COMPANY_ID,
@@ -166,8 +149,19 @@ static const struct bt_mesh_comp comp = {
 	.elem_count = ARRAY_SIZE(elements),
 };
 
+void model_handler_set_state(uint16_t addr, uint8_t state)
+{
+    int err;
 
-uint16_t count=0;
+    enum bt_mesh_gus_cli_state gus_state = state;
+        printk("identify %d\n", addr);
+    err = bt_mesh_gus_cli_state_set(&gus, addr, gus_state);
+    if (err) {
+        printk("identify %d %d failed %d\n", addr,state, err);
+    }
+}
+
+
 static void button_handler_cb(uint32_t pressed, uint32_t changed)
 {
     if (!bt_mesh_is_provisioned()) {
@@ -175,10 +169,12 @@ static void button_handler_cb(uint32_t pressed, uint32_t changed)
     }
 
     if ((pressed & changed & BIT(1))) {
-        bt_mesh_reset();
+ //       bt_mesh_reset();
+         model_handler_set_state(3, 1);
         dk_set_leds(1);
     }
     else {
+         model_handler_set_state(4, 1);
         dk_set_leds(2);
 
     }
